e4979c01fc86cb9bd21a6bdf61f2b3979274d1c2
daqi <daqidaqi@qq.com>
2014-10-16 12:36:21 +0800

    pcupdate 除弹窗等js基本完

--- demopc/js/main.js
+++ demopc/js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+    $(".back").click(function(){
+        history.back(-1);
+    });
+    $(".guizetabs div").click(function(){
+        if (!$(this).hasClass("on")) {
+            var index = $(this).index();
+            $(this).addClass("on").siblings().removeClass("on");
+            $(".guizeli").hide();
+            $(".guizeli").eq(1-index).show();
+        };
+    });
     if ($(".imageswitch1").length) {
         $(".imageswitch1").slide({
             titCell: ".list li",
@@ -10,6 +21,8 @@ $(function() {
             trigger: "click",
             easing: "easeOutCirc"
         });
+    };
+    if ($(".imageswitch2").length) {
         $(".imageswitch2").slide({
             titCell: ".list li",
             mainCell: ".piclist2",
@@ -23,20 +36,20 @@ $(function() {
     };
 });
 
-$(function() {
-    if ($(".haibaodetailimageswitch").length) {
-        $(".haibaodetailimageswitch").slide({
-            titCell: ".list li",
-            mainCell: ".piclist3",
-            effect: "left",
-            vis: 5,
-            scroll: 5,
-            delayTime: 800,
-            trigger: "click",
-            easing: "easeOutCirc"
-        });
-    };
-});
+// $(function() {
+//     if ($(".haibaodetailimageswitch").length) {
+//         $(".haibaodetailimageswitch").slide({
+//             titCell: ".list li",
+//             mainCell: ".piclist3",
+//             effect: "left",
+//             vis: 5,
+//             scroll: 5,
+//             delayTime: 800,
+//             trigger: "click",
+//             easing: "easeOutCirc"
+//         });
+//     };
+// });
 
 $(function() {
     $(".nav .nav5").on("mouseover", function() {
